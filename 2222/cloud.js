10058e1175117111acf5ea752b6b341059d8c630
bogdan-molodets <bogdan.molodets@gmail.com>
2020-08-12 10:44:30 +0300

    add test 3d files

---
+++ 2222/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2087381,
+    "boundingBox": {
+        "lx": -1.782425880432129,
+        "ly": -7.474335670471191,
+        "lz": -20.856910705566408,
+        "ux": 41.93939971923828,
+        "uy": 36.24748992919922,
+        "uz": 22.864914894104005
+    },
+    "tightBoundingBox": {
+        "lx": -1.782,
+        "ly": -7.474,
+        "lz": -20.857,
+        "ux": 41.939,
+        "uy": 12.876,
+        "uz": -9.868
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.37864211201667788,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
